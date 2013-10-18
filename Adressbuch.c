8038d906f434b859ad367ea4240881d9e8d2d7fb
Andreas Vogt <andi6444@hotmail.com>
2013-10-18 10:49:52 +0200

    erster commit

---
+++ Adressbuch.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define stelle = 1
+struct Person ()
+struct Adress ()
+void hinzufuegen(char[],char[])
+void ausgeben()
+
+
+int main (int argc , char* argv[])
+{
+	  printf("Geben sie einen Personennamen ein");
+	  scanf("%c",&z);
+}
+
+struct Person ()
+{
+	char * name ;
+	name = mailloc(1+sizeof(*name)),
+	
+	free(name);
+}p1;
+struct Adress (char[]){
+	char * adresse,
+	adresse = mailloc(sizeof(*adresse),
+	free(adresse)																																																		
+}a1;
+
+void hinzufuegen (char[] name,char[] adress){
+	int i;
+	p1.name[stelle];
+	stelle ++;
+	a1.adresse[stelle];
+}
+
+void ausgeben (){
+}
