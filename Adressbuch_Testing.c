c715b786f0abe44513c337aefbec03bcb627ac82
wmair-tgm <wmair@student.tgm.ac.at>
2013-11-04 16:44:44 +0100

    a new try

---
+++ Adressbuch_Testing.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void hinzufuegen(char* namet[], char* adresset[]);
+void ausgeben();
+
+typedef struct
+{
+	char n[100];
+	char* name;
+	int anz;
+	name = n;
+	name = mailloc(sizeof(*name));
+	free(name);
+}person;
+
+typedef struct{
+	char a[100];
+	char* adresse;
+	int anz;
+	adresse = a;
+	name = malloc(sizeof(*adresse));
+	free(adresse);																																																		
+}adress;
+
+int main (int argc , char* argv[])
+{
+	person* p;
+	adress* a;
+
+	int stelle = 0;
+	char textn[100];
+	char texta[100];
+	int anzN;
+	int anzA;
+	
+	printf("Geben sie einen Personennamen ein");
+	anzN = scanf("%s",textn);
+	printf("Geben sie einen Adresse ein");
+	anzA = scanf("%s",texta);
+	  
+	  
+}
+
+void hinzufuegen (char* namet[],char* adresset[]){
+	p = realloc(p,1+sizeof(person));
+	int i;
+	size_t ln;
+	ln = strlen(namet);
+	size_t la;
+	la = strlen(adresset);
+	for(i=0;i<ln;i++){
+		p[stelle].name[i] = namet[i] ;
+		p[stelle].name = malloc(1+sizeof(*name));
+	}
+	for(i=0;i<la;i++){
+		a[stelle].adresse[i]  = adresset[i];
+		a[stelle].adresse = malloc(1+sizeof(*adresse);
+	}
+	stelle++;
+}
+
+void ausgeben (){
+
+	for(int i=0; i<stelle; i++){
+		for(int z=0; z<p[i].anz;z++){
+			printf("%c",p[i].name[z]);
+		}
+		for(int z=0; z<p[i].anz;z++){
+			printf("%c",a[i].adresse[z]);
+		}
+		printf("\n");
+	}
+}
